fba46c0a56de8e11517f43adf5af5ada40b30a53
RoniJunior <ronijunior996@gmail.com>
2024-04-29 14:06:29 -0300

    python1

---
+++ sorteio/sort.py
@@ -0,0 +1,14 @@
+from kivymd.app import MDApp
+from kivymd.uix.floatlayout import MDFloatLayout
+from servico import Servico
+
+class Tela(MDFloatLayout):
+    def sortearLista(self):
+        result = self.ids.lista.text.split(",")
+        result = Servico.sortear(result)
+        self.ids.lb.text = result
+
+class App(MDApp):
+    pass
+if __name__=="__main__":
+    App(). run()
